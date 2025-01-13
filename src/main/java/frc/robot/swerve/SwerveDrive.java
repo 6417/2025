19b9be4921a34acb2ac8a42b0c0803221f68fc5b
Gian-Laager <gianlaager114@gmail.com>
2025-01-13 19:37:51 +0100

    add: swerve dirve subsystem with basic functionality.

---
+++ src/main/java/frc/robot/swerve/SwerveDrive.java
@@ -0,0 +1,59 @@
+package frc.robot.swerve;
+
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
+import edu.wpi.first.math.kinematics.SwerveModuleState;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import frc.fridowpi.motors.FridolinsMotor.IdleMode;
+
+public class SwerveDrive {
+    public SwerveModule[] modules;
+    private SwerveDriveKinematics kinematics;
+
+    public static final int LOC_FL = 0;
+    public static final int LOC_FR = 1;
+    public static final int LOC_RL = 2;
+    public static final int LOC_RR = 3;
+
+    public SwerveDrive(ModuleConfig[] configs) {
+        String[] moduleNames = new String[4];
+        moduleNames[LOC_FR] = "Front Right";
+        moduleNames[LOC_FL] = "Front Left";
+        moduleNames[LOC_RR] = "Rear Right";
+        moduleNames[LOC_RL] = "Rear Left";
+
+        for (int i = 0; i < 4; i++) {
+            configs[i].name = moduleNames[i];
+            modules[i] = new SwerveModule(configs[i]);
+            Shuffleboard.getTab("Drive").add("SwerveModule" + moduleNames[i], modules[i]);
+        }
+
+        kinematics = new SwerveDriveKinematics(
+                configs[0].moduleOffset,
+                configs[1].moduleOffset,
+                configs[2].moduleOffset,
+                configs[3].moduleOffset);
+    }
+
+    public void setChassisSpeeds(ChassisSpeeds speeds) {
+        SwerveModuleState[] moduleStates = kinematics.toSwerveModuleStates(speeds);
+
+        for (int i = 0; i < 4; i++) {
+            modules[i].setDesiredState(moduleStates[i]);
+        }
+    }
+
+    public void stopMotors() {
+        for (var module : modules) {
+            module.stopMotors();
+        }
+    }
+
+    public void setIdleMode(IdleMode mode) {
+        for (var module : modules) {
+            module.setIdleMode(mode);
+        }
+    }
+}
