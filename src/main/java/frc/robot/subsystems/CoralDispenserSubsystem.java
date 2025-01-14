40db835d18366f9b68b8a8548ebe619f38a8bb77
Janis Gätzi <janis.g@6417.ch>
2025-01-14 20:35:31 +0100

    Adding Liftingtower Subsystem$

--- src/main/java/frc/robot/subsystems/CoralDispenserSubsystem.java
+++ src/main/java/frc/robot/subsystems/CoralDispenserSubsystem.java
@@ -1,14 +1,18 @@
 package frc.robot.subsystems;
 
 public class CoralDispenserSubsystem {
+    private CoralDispenserSubsystem
+
+    public static CoralDispenserSubsystem getInstance() {
+        if (instance == null) {
+            instance = new CoralDispenserSubsystem();
+
+        }
+    }
+
+    private CoralDispenserSubsystem() {
     
-    public CoralDispenserSubsystem() {
     }
 
-    public void periodic() {
-    }
-
-    public void simulationPeriodic() {
-    }
     
 }
--- src/main/java/frc/robot/subsystems/LiftingTowerSubsystem.java
+++ src/main/java/frc/robot/subsystems/LiftingTowerSubsystem.java
@@ -1,17 +1,55 @@
 package frc.robot.subsystems;
 
-import frc.fridowpi.motors.FridoFalcon500v6;
+import java.nio.channels.UnsupportedAddressTypeException;
+import java.util.function.LongFunction;
+import java.util.logging.Logger;
 
-public class LiftingTowerSubsystem {
-    private FridoFalcon500v6 hubturmMotorLinks;
-    private FridoFalcon500v6 hubturmMotorRechts;
-    public LiftingTowerSubsystem() {
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+import frc.fridowpi.motors.FridoSparkMax;
+import frc.fridowpi.motors.FridolinsMotor.DirectionType;
+import frc.fridowpi.motors.utils.PidValues;
+import frc.robot.Constants;
+
+public class LiftingTowerSubsystem extends SubsystemBase {
+    private static LiftingTowerSubsystem instance;
+
+    private final PidValues pidValues = new PidValues(0, 0, 0, 0); // p, i, d, f
+            
+    private FridoSparkMax motorLeft;
+    private FridoSparkMax motorRight;
+    
+    public static LiftingTowerSubsystem getInstance() {
+        if (instance == null) {
+            instance = new LiftingTowerSubsystem();
+        }
+        
+        return instance;
     }
 
-    public void periodic() {
-    }
-
-    public void simulationPeriodic() {
+    private LiftingTowerSubsystem() {
+        motorLeft = new FridoSparkMax(Constants.LiftingTower.liftingTowerLeftId);
+        motorRight = new FridoSparkMax(Constants.LiftingTower.liftingTowerRightId);
+        
+        motorLeft.follow(motorRight, DirectionType.invertMaster);
+        
+        motorRight.setPID(pidValues);
     }
     
+    public void startMotors(double speed) {
+        if (speed < -1.0 || speed > 1.0) {
+            throw new IllegalArgumentException("Set speed between -1.0 and 1.0");
+        }
+
+        motorRight.set(speed);
+    }
+
+    public void setPosition(double position) {
+        motorRight.setPosition(position);
+    }
+
+    public void stopMotors() {
+        startMotors(0);
+    }
 }
