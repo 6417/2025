91a1a5e68a3d8cf8ca29d2a5c072f70df07b3d6a
Eragon24z <dracheeragon3@gmail.com>
2025-01-14 17:59:49 +0100

    Add 3 Subsystems

---
+++ src/main/java/frc/robot/subsystems/SkiliftSubsystem.java
@@ -0,0 +1,17 @@
+package frc.robot.subsystems;
+
+import frc.fridowpi.motors.FridoFalcon500v6;
+
+public class SkiliftSubsystem {
+    private FridoFalcon500v6 hubturmMotorLinks;
+    private FridoFalcon500v6 hubturmMotorRechts;
+    public SkiliftSubsystem() {
+    }
+
+    public void periodic() {
+    }
+
+    public void simulationPeriodic() {
+    }
+    
+}
