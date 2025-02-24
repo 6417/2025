3624edff00c4e58d8ff44eedc05abe8a08811d92
Gian-Laager <gianlaager114@gmail.com>
2025-02-24 22:18:35 +0100

    add condition for at height in CoralGoToHeightState

--- src/main/java/frc/robot/commands/LiftingTower/CoralGoToHeightState.java
+++ src/main/java/frc/robot/commands/LiftingTower/CoralGoToHeightState.java
@@ -33,6 +33,6 @@ public class CoralGoToHeightState extends Command {
 
     @Override
     public boolean isFinished() {
-        return liftingTowerSubsystem.isAtDesiredHeight();
+        return liftingTowerSubsystem.isAtDesiredHeight() || liftingTowerSubsystem.isAtDesiredHeight();
     }
 }
